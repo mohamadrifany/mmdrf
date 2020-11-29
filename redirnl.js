b476c3b94e204d1043b0d377346753ba00f63cc8
mohamadrifany <70678421+mohamadrifany@users.noreply.github.com>
2020-11-29 12:12:13 +0700

    Update redirnl.js

--- redirnl.js
+++ redirnl.js
@@ -4,7 +4,7 @@ var key = document.URL.split("/")[4];
 var id = document.URL.split("/")[5];
 
 if (md == 'nl') {
-  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://nl.aliexpress.com/item/'+id+'';
+  var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://www.nl.aliexpress.com/item/'+id+'';
 } else {
   var redir = 'https://s.click.aliexpress.com/e/'+key;
 }
