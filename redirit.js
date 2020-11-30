9c1e0cc862c2878e29535365ffca42971d710941
mohamadrifany <70678421+mohamadrifany@users.noreply.github.com>
2020-11-30 09:46:09 +0700

    Update redirit.js

--- redirit.js
+++ redirit.js
@@ -3,7 +3,7 @@ var md = document.URL.split("/")[3];
 var key = document.URL.split("/")[4];
 var id = document.URL.split("/")[5];
 
-if (md == 'nl') {
+if (md == 'it') {
   var redir = 'http://s.click.aliexpress.com/deep_link.htm?aff_short_key='+key+'&dl_target_url=https://it.aliexpress.com/item/'+id+'';
 } else {
   var redir = 'https://s.click.aliexpress.com/e/'+key;
